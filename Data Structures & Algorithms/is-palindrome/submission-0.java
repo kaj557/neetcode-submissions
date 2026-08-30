f43f1aa83960b60042d8830611287be87643578e
Kajal Kumari <btech60121.24@bitmesra.ac.in>
2026-08-30 19:15:26 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder str = new StringBuilder();
+
+        
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                str.append(Character.toLowerCase(c));
+            }
+        }
+
+        
+        String original = str.toString();
+        String reversed = str.reverse().toString();
+
+        return original.equals(reversed);
+    }
+}
